d4f3cde33362aa59d3623c6665da9e70aa9eae77
CarvalhoVincent <carvalho.vincent@hotmail.fr>
2022-05-16 14:46:23 +0200

    better function

--- script.js
+++ script.js
@@ -1,30 +1,11 @@
 const rating = ["0"];
 
 
-function saveRate1() {
+function saveRate(el) {
     rating.pop();
-    rating.push(document.getElementById("1").innerHTML);
+    rating.push(el.value);
 }
 
-function saveRate2() {
-    rating.pop();
-    rating.push(document.getElementById("2").innerHTML);
-}
-
-function saveRate3() {
-    rating.pop();
-    rating.push(document.getElementById("3").innerHTML);
-}
-
-function saveRate4() {
-    rating.pop();
-    rating.push(document.getElementById("4").innerHTML);
-}
-
-function saveRate5() {
-    rating.pop();
-    rating.push(document.getElementById("5").innerHTML);
-}
 
 function displayThanks() {
     var thanksPage = document.getElementById("thanks");
